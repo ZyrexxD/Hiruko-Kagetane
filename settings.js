f750f7c93c192344b0eab29ea9c93160e1b78115
ZyrexxD <106370609+ZyrexxD@users.noreply.github.com>
2022-05-28 10:26:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,28 +12,28 @@ global.APIKeys = {
 
 //===> ATUR BIAR KALIAN SENENG
 global.autoReadGc = true //Ubah Ke false Jika Chat Di Grup Tidak Ingin Di Baca Oleh Bot
-global.autoReadAll = false //Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
+global.autoReadAll = true //Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
 global.anticall = true //Ubah Ke false Jika Tidak Ingin Bot Blockir Yang Telepon
 global.available = false //Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
-global.autoTyping = false //Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
+global.autoTyping = true //Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
 global.autoRecord = true //Ubah Ke false Jika Tidak Ingin Saat Ada Yang Chat Bot Auto Merekam
 global.multiplier = 69 // Semakin Besar Angkanya Semakin Sulit Untuk Naik Level
 global.limitCount = 70 //70 Dah Pas Banh
 global.prefa = ['','.','/','#']
 
 //===> UBAH DISINI
-global.namaowner = 'Ryuuka Team' //Ganti Jadi Namamu
-global.owner = ['6285609233482','6283891921787'] //Kalo Mau Lebih Tambahin Aja
-global.nomerowner = '6285609233482' //Owner Utama Istilahnya
-global.namabot = 'Hiruko Kagetane' //Ganti Jadi Nama Botmu
-global.packname = 'Hiruko Kagetane' //Sticker Weem
-global.author = 'By Ryuuka Team' //Sticker Weem
+global.namaowner = 'DenzzTeam' //Ganti Jadi Namamu
+global.owner = ['6285723219491','6285723219491'] //Kalo Mau Lebih Tambahin Aja
+global.nomerowner = '6285723219491' //Owner Utama Istilahnya
+global.namabot = 'TeamD' //Ganti Jadi Nama Botmu
+global.packname = 'DenzzSticker' //Sticker Weem
+global.author = 'By DenzzTeam' //Sticker Weem
 global.webme = 'https://api-ryubotz.herokuapp.com' //Ganti Sama Web Mu
-global.linkgroup = 'https://chat.whatsapp.com/BhOYdoWNRFC4n4bdsEexlV' //Ganti Sama Link Gc Mu
+global.linkgroup = '-' //Ganti Sama Link Gc Mu
 global.sessionName = 'session'
-global.youtube = 'https://youtube.com/c/RyuukaBotz' //Ganti Sama Link Yt Mu
-global.github = 'https://github.com/R-TEAM94' //Ganti Sama Link Github Mu
-global.instagram = 'https://instagram.com/fandyyy._' //Ganti Sama Link Ig mu
+global.youtube = 'https://youtube.com/c/DeniezYT' //Ganti Sama Link Yt Mu
+global.github = 'https://github.com/DenzzJB' //Ganti Sama Link Github Mu
+global.instagram = '-' //Ganti Sama Link Ig mu
 
 //===> BACKGROUND SETIAP MENU
 global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
